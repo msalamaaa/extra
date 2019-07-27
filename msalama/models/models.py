2a8b5f994080d8a77e0bfc5634222051f37e6d31
msalamaaa <msalamaaa@gmail.com>
2019-07-27 13:39:54 +0200

    m

---
+++ msalama/models/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class msalama(models.Model):
+    _name = 'msalama.msalama'
+
+    name = fields.Char('the name')
+    phone = fields.Integer('phone')
+
+
+class calories(models.Model):
+    _name = 'product.template'
+    _inherit = 'product.template'
+
+
+    calories = fields.Integer('calories')
+    service = fields.Float('service')
+    lastupdate = fields.Date('last update')
+    update = fields.Date('update')
+    size = fields.Float('size')
+    dietitem = fields.Boolean('diet item')
+
+
+#class nickname(models.Model):
+   # _name = 'res.partner'
+   # _inherit = 'res.partner'
+
+    #nickname = fields.Integer('nickname')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
